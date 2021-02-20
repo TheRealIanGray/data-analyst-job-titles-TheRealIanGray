3b7a40f357c6e3fb70b1f6bbf0fe685a29d8c3d4
TheRealIanGray <therealiangray@gmail.com>
2021-02-20 12:26:30 -0600

    finished

--- data/project_start.sql
+++ data/project_start.sql
@@ -1,21 +1,25 @@
 /*select count(*) from data_analyst_jobs;
 3586*/
 
+/*quiestion 1*/
 /*select *
 from data_analyst_jobs
 limit 10;
 "ExxonMobil"*/
 
+/*question 2*/
 /*select count(title)
 from data_analyst_jobs
 where location = 'TN';
 42*/
 
+/*question 3*/
 /*select count(title)
 from data_analyst_jobs
 where location = 'TN' or location = 'KY';
 54*/
 
+/*question 4*/
 /*select count(title)
 from data_analyst_jobs
 where star_rating > 3.9999999999;
@@ -45,4 +49,36 @@ where location = 'CA';
 752*/
 
 /*question 9*/
+/*select company, avg(star_rating) as rating, sum(review_count)
+from data_analyst_jobs
+where review_count > 5000
+group by company
+order by rating desc*/
 
+/*question 10*/
+/*select company, avg(star_rating) as rating, sum(review_count)
+from data_analyst_jobs
+where review_count > 5000
+group by company
+order by rating desc*/
+
+/*question 11*/
+/*select distinct(title) 
+from data_analyst_jobs
+where title like '%Analyst%'*/
+/*3272*/
+
+/*question 12*/
+/*select distinct(title)
+from data_analyst_jobs
+where title not ilike '%Analyst%'
+and title not ilike '%Analytics%'*/
+
+/*Bonus*/
+select count(title)/2 as title_count, domain
+from data_analyst_jobs
+where days_since_posting > '21'
+and skill ilike '%sql%'
+and domain not like 'null'
+group by domain
+order by title_count desc
